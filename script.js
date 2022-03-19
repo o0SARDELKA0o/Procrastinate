f41cdae79b771432c00da65f251fa7d605837cfd
o0SARDELKA0o <95570559+o0SARDELKA0o@users.noreply.github.com>
2022-03-19 12:31:45 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+let phrases = [
+  { text: 'отправить другу смешную гифку', image: 'https://code.s3.yandex.net/web-code/procrastinate/1.gif' },
+  { text: 'поиграть в популярные игры', image: '2.png' },
+  { text: 'скрафтить незеритовую мотыгу и не понять зачем она тебе', image: '3.png' },
+  { text: 'поиграть в Роблокс', image: '4.png' },
+  { text: 'посмотреть смешные мемасики', image: '5.png' },
+  { text: 'посмотреть скидки на авиабилеты', image: 'https://code.s3.yandex.net/web-code/procrastinate/2.png' },
+  { text: 'разобраться, о чём поют рэперы', image: 'https://code.s3.yandex.net/web-code/procrastinate/3.png' },
+  { text: 'Юрий Дудь', image: 'https://code.s3.yandex.net/web-code/procrastinate/4.png' },
+  { text: 'расставить книги на полке по цвету', image: 'https://code.s3.yandex.net/web-code/procrastinate/5.png' },
+  { text: 'читать про зарплаты в Сан-Франциско', image: 'https://code.s3.yandex.net/web-code/procrastinate/6.png' },
+  { text: 'прочитать новости и ужаснуться в комментариях', image: 'https://code.s3.yandex.net/web-code/procrastinate/7.png' },
+  { text: 'попасть в поток грустных песен и вспомнить все ошибки молодости', image: 'https://code.s3.yandex.net/web-code/procrastinate/8.png' },
+  { text: 'посмотреть трейлер сериала и заодно первый сезон', image: 'https://code.s3.yandex.net/web-code/procrastinate/9.png' },
+  { text: 'не забыть погладить кошку!', image: '1.png' },
+  { text: 'проверить непрочитанное в Telegram-каналах', image: 'https://code.s3.yandex.net/web-code/procrastinate/10.png' }
+];
+
+function getRandomElement(arr) {
+  let randIndex = Math.floor(Math.random() * arr.length);
+  return arr[randIndex];
+}
+
+let button = document.querySelector('.button');
+let phrase = document.querySelector('.phrase');
+let advice = document.querySelector('.advice');
+let image = document.querySelector('.image');
+
+button.addEventListener('click', function () {
+  let randomElement = getRandomElement(phrases);
+  smoothly(phrase, 'textContent', randomElement.text)
+  smoothly(image, 'src', randomElement.image)
+
+  if (randomElement.text.length > 40) {
+    advice.style.fontSize = '33px';
+  } else {
+    advice.style.fontSize = '42px';
+  }
+});
+
+for (let i = 0; i <= 2; i = i + 1) {
+  // выводите элементы массива в консоль, используя i
+ // console.log(phrases[i]);
+  smoothly(phrase, 'textContent', phrases[i].text); 
+  smoothly(image, 'src', phrases[i].image); 
+} 
